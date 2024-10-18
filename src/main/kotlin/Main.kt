f2711b78b7f268f80d9997d120f29648b2c0db63
Aleksei Kurazhov <mob_new@mail.ru>
2024-10-18 17:34:12 +0300

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,75 @@
+package ru.netology
+
+import kotlin.random.Random
+
+data class Comments(
+    val count: Int,
+    val canPost: Boolean,
+    val canGroupPost: Boolean,
+    val canClose: Boolean,
+    val canOpen: Boolean
+)
+
+data class Likes(
+    val count: Int,
+    val userLikes: Boolean,
+    val canLike: Boolean,
+    val canPublish: Boolean
+)
+
+data class Reposts(
+    val count: Int,
+    val userReposted: Boolean
+)
+
+data class Post(
+    val id: Int = 0,
+    val ownerId: Int = 0,
+    val text: String,
+    val friendsOnly: Boolean = false,
+    val comments: Comments = Comments(0, true,true,true,true),
+    val likes: Likes = Likes(0, false, true, true),
+    val reposts: Reposts = Reposts(0, false),
+    val views: Int = 0,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true
+)
+
+object WallService {
+    var arrayPosts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        val tmpPost = post.copy(id = arrayPosts.count() + 1, ownerId = Random.nextInt(1,100_000))
+        arrayPosts += tmpPost
+        return arrayPosts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, postArr) in arrayPosts.withIndex()) {
+            if (post.id == postArr.id) {
+                arrayPosts[index] = post
+                return true
+            }
+        }
+        return false
+    }
+
+    fun clearArray() {
+        arrayPosts = emptyArray()
+    }
+}
+
+
+fun main() {
+    WallService.add(Post(text = "test post 1"))
+    WallService.add(Post(text = "test post zzzzzzzzzz"))
+    WallService.add(Post(text = "test post ppppppppppp"))
+
+//    println(WallService.getAll().contentToString())
+
+    val postForEdit = Post(id = 2, ownerId = 111, text = "changed text", friendsOnly = true, canPin = false, canDelete = false)
+    WallService.update(postForEdit)
+
+//    println(WallService.getAll().contentToString())
+
+}
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,44 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import ru.netology.Post
+import ru.netology.WallService
+
+class WallServiceTest {
+
+    @Test
+    fun addTest() {
+        val post = WallService.add(Post(text = "test post 1"))
+        assertNotEquals(0, post.id)
+    }
+
+    @Before
+    fun clearBeforeTest() {
+        WallService.clearArray()
+    }
+
+    @Test
+    fun updateTestPositive() {
+        WallService.add(Post(text = "test post 1"))
+        WallService.add(Post(text = "test post zzzzzzzzzz"))
+        WallService.add(Post(text = "test post ppppppppppp"))
+
+        val postForEdit = Post(id = 2, ownerId = 111, text = "changed text", friendsOnly = true, canPin = false, canDelete = false)
+        val updatePost = WallService.update(postForEdit)
+
+        assertTrue(updatePost)
+    }
+
+    @Test
+    fun updateTestNegative() {
+        WallService.add(Post(text = "test post 1"))
+        WallService.add(Post(text = "test post zzzzzzzzzz"))
+        WallService.add(Post(text = "test post ppppppppppp"))
+
+        val postForEdit = Post(id = 20, ownerId = 111, text = "changed text", friendsOnly = true, canPin = false, canDelete = false)
+        val updatePost = WallService.update(postForEdit)
+
+        assertFalse(updatePost)
+    }
+}
